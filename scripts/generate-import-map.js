94865b8587336bdcbd07435a9fd688af19cea13d
Phil Pluckthun <phil@kitten.sh>
2021-12-13 17:49:38 +0000

    Rewrite import map generator to automatically resolve conflicts
    This enables the import map generator to automatically trace
    graphql v14, v15, and v16 and finds the most specific common
    import declaration between all three of them, if possible. As
    a bonus, it now excludes exports that aren't available in all
    three versions.

--- scripts/generate-import-map.js
+++ scripts/generate-import-map.js
@@ -3,116 +3,142 @@ const path = require('path');
 
 const { rollup } = require('rollup');
 
-const cwd = process.cwd();
-const basepath = path.resolve(cwd, 'node_modules/graphql/');
+/** Generates a map of exports from a given graphql package to list of import locations. */
+async function traceImports(moduleName) {
+  const basepath = path.resolve(process.cwd(), 'node_modules/', moduleName);
+  const exportMap = {};
 
-function generateImportMapPlugin(opts = {}) {
-  const maxDepth = opts.maxDepth || 2;
-  const filename = opts.filename || 'import-map.json';
-  const map = new Map();
-
-  const resolveFile = (from, to) => {
-    return path.join(from, to);
+  const resolveFile = (to, relative = '.') => {
+    const dirname = path.join('graphql/', relative, path.dirname(to));
+    const filename = path.basename(to, '.mjs');
+    return filename !== 'index' ? path.join(dirname, filename) : dirname;
   };
 
-  const resolveFromMap = (id, name, depth = 0) => {
-    const exports = map.get(id);
-    if (!exports || !exports.has(name)) return null;
-
-    const declaration = exports.get(name);
-    if (depth >= maxDepth || declaration.from === id) {
-      return declaration;
-    }
-
-    return resolveFromMap(declaration.from, declaration.local, depth + 1) || declaration;
-  };
-
-  return {
-    name: 'generate-import-map',
-    transform(code, id) {
-      const relative = path.relative(basepath, id);
-      const dirname = path.dirname(relative);
-      const exports = new Map();
-
-      this.parse(code)
-        .body.filter((x) => x.type === 'ExportNamedDeclaration')
-        .forEach((node) => {
-          const source = node.source ? resolveFile(dirname, node.source.value) : relative;
-
-          node.specifiers.forEach((specifier) => {
-            exports.set(specifier.exported.name, {
-              local: specifier.local.name,
-              from: source,
-            });
-          });
-
-          if (node.declaration) {
-            (node.declaration.declarations || [node.declaration]).forEach((declaration) => {
-              if (declaration && declaration.id) {
-                const { name } = declaration.id;
-                exports.set(declaration.id.name, {
-                  local: name,
-                  from: source,
-                });
-              }
-            });
-          }
-        });
-
-      map.set(relative, exports);
-      return null;
-    },
-    renderChunk(_code, chunk) {
-      const id = chunk.facadeModuleId;
-      const relative = path.relative(basepath, id);
-
-      if (chunk.isEntry) {
-        const importMap = chunk.exports.reduce((acc, name) => {
-          const declaration = resolveFromMap(relative, name);
-          if (declaration) {
-            const dirname = path.join('graphql/', path.dirname(declaration.from));
-            const filename = path.basename(declaration.from, '.mjs');
-
-            acc[name] = {
-              local: declaration.local,
-              from: path.join(dirname, filename),
-            };
-          }
-
-          return acc;
-        }, {});
-
-        this.emitFile({
-          type: 'asset',
-          filename,
-          name: filename,
-          source: JSON.stringify(importMap, null, 2),
-        });
-      }
-    },
-  };
-}
-
-(async () => {
   const bundle = await rollup({
-    input: path.join(basepath, 'index.mjs'),
+    // This contains all top-level "sub-modules" of graphql too, since not all exports of
+    // them may be exposed in the main index.mjs file.
+    input: {
+      graphql: path.join(basepath, 'index.mjs'),
+      'graphql/error': path.join(basepath, 'error/index.mjs'),
+      'graphql/execution': path.join(basepath, 'execution/index.mjs'),
+      'graphql/language': path.join(basepath, 'language/index.mjs'),
+      'graphql/subscription': path.join(basepath, 'subscription/index.mjs'),
+      'graphql/type': path.join(basepath, 'type/index.mjs'),
+      'graphql/utilities': path.join(basepath, 'utilities/index.mjs'),
+      'graphql/validation': path.join(basepath, 'validation/index.mjs'),
+    },
     external: (id) => !/^\.{0,2}\//.test(id),
     preserveModules: true,
     plugins: [
       require('@rollup/plugin-node-resolve').nodeResolve(),
-      generateImportMapPlugin({
-        filename: 'import-map.json',
-      }),
+      {
+        transform(code, id) {
+          const relative = path.relative(basepath, id);
+          const dirname = path.dirname(relative);
+          const exports = {};
+
+          this.parse(code)
+            .body.filter((x) => x.type === 'ExportNamedDeclaration')
+            .forEach((node) => {
+              const from = node.source
+                ? resolveFile(node.source.value, dirname)
+                : resolveFile(relative);
+
+              node.specifiers.forEach((specifier) => {
+                const { name: local } = specifier.exported;
+                exports[local] = { local, from };
+              });
+
+              if (node.declaration) {
+                (node.declaration.declarations || [node.declaration]).forEach((declaration) => {
+                  if (declaration && declaration.id) {
+                    const { name: local } = declaration.id;
+                    exports[local] = { local, from };
+                  }
+                });
+              }
+            });
+
+          exportMap[resolveFile(relative)] = exports;
+          return null;
+        },
+      },
     ],
   });
 
-  const { output } = await bundle.generate({});
+  await bundle.generate({});
+  return exportMap;
+}
 
-  fs.writeFileSync(
-    path.resolve(cwd, 'import-map.json'),
-    output.find((asset) => asset.type === 'asset').source
+function isDeclarationEqual(a, b) {
+  return a.local === b.local && a.from === b.from;
+}
+
+function mergeTraces(traces) {
+  const trace = {};
+
+  // Iterate over all known filenames in all traces
+  const ids = new Set(
+    traces.map((trace) => Object.keys(trace)).reduce((acc, names) => acc.concat(names), [])
   );
+  for (const id of ids) {
+    // Each file must exist in all traces
+    if (!traces.every((trace) => !!trace[id])) continue;
+
+    const exports = {};
+
+    // Iterate over all known exports in each trace's set of exports for this file
+    const exportNames = new Set(
+      traces.map((trace) => Object.keys(trace[id])).reduce((acc, names) => acc.concat(names), [])
+    );
+    for (const name of exportNames) {
+      // Each export must exist in all traces
+      if (traces.every((trace) => !!trace[id][name])) {
+        // Collect known declarations and deduplicate
+        exports[name] = traces
+          .map((trace) => trace[id][name])
+          .filter((val, index, all) => {
+            const firstIndex = all.findIndex((item) => isDeclarationEqual(item, val));
+            return firstIndex === index;
+          });
+      }
+    }
+
+    if (Object.keys(exports).length) trace[id] = exports;
+  }
+
+  // For a given declaration, find the first deepest one that works for the trace
+  // NOTE: This doesn't find the absolute deepest one, since it assumes that each
+  // export only has one functional trace
+  const resolveDeclaration = (declaration) => {
+    const declarations = trace[declaration.from];
+    if (!declarations || !declarations[declaration.local]) return null;
+    for (const childDeclaration of declarations[declaration.local]) {
+      if (childDeclaration.from === declaration.from) continue;
+      const resolved = resolveDeclaration(childDeclaration);
+      if (resolved && resolved.from !== declaration.from) return resolved;
+    }
+
+    return declaration;
+  };
+
+  // Resolve all known (and consistent) exports to a common, deepest declaration
+  for (const local in trace.graphql)
+    exports[local] = resolveDeclaration({ local, from: 'graphql' });
+  return exports;
+}
+
+(async () => {
+  const traces = await Promise.all([
+    traceImports('graphql-14'),
+    traceImports('graphql-15'),
+    traceImports('graphql-16'),
+  ]);
+
+  const trace = mergeTraces(traces);
+
+  fs.writeFileSync('import-map.json', JSON.stringify(trace, null, 2));
 })().catch((err) => {
-  console.error(`${err.name}: ${err.message}`);
+  console.error(`${err.name}: ${err.stack}`);
   process.exit(1);
 });
